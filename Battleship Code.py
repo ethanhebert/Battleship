774ac4f9187ddf807d4aac797078eb44ae54932d
ethanhebert <80844614+ethanhebert@users.noreply.github.com>
2021-04-21 22:34:16 -0500

    Add files via upload

--- Battleship Code.py
+++ Battleship Code.py
@@ -28,6 +28,7 @@ class Game(Frame):
         self.left.grid(row=1, column=0, rowspan=10)
         
         #left grid
+        global img
         for i in range(0,8):
             for j in range(0,8):
                 #j is x, i is y
@@ -153,18 +154,139 @@ class Game(Frame):
             self.label.image = img
             self.label.grid(row=y, column=x, sticky = NSEW)
 
+
+def titlescreen():
+    global title
+    global titleStatus
+
+    if (titleStatus == 0):
         
+        title.attributes("-fullscreen", True)
+
+        title_label = Label(title, text="BATTLESHIP", font=("Helvetica", 18))
+        title_label.pack()
+
+    title.update_idletasks() #same as mainloop but allows frame to update
+    title.update()
+
+    #start the title music
+    sounds[0].play(loops=-1)
+
+    #have to let go of blue button then press it for it to start (no holding button)
+    while True:
+        if (GPIO.input(buttons[0]) == False):
+            sleep(0.05)
+            while True:
+                if (GPIO.input(buttons[0]) == True):
+                    sounds[0].stop()
+                    #title.destroy()
+                    break
+            break
+
+
+
+def gameloop():
+
+    #start by displaying the title screen
+    titlescreen()
+
+    global title
+    global window
+    global b1
+    global squareStatus
+    global titleStatus
+
+    #initial dictionary storing status (hit, miss, or regular) of every square "xy" in grid
+    squareStatus = {}
+
+    #make the game window appear now
+    window = Toplevel(title)
+    b1 = Game(window)
+    b1.setupGUI()
+    
+    #start an in-game song
+    sounds[1].play(loops=-1)
+
+    #make an initial square to make green
+    currentx = 4
+    currenty = 4
+    b1.green(currentx, currenty)
+
+    #render the GUI in the main loop
+    #window.mainloop()
+    window.update_idletasks() #same as mainloop but allows frame to update
+    window.update()
+
+    #have to let go of blue button to start game
+    while True:
+        if (GPIO.input(buttons[0]) == False):
+            break
+
+    #MAIN GAME LOOP#
+    while True:
+        #test if the joystick is being moved any direction and go that direction
+        #joystick is normally 1 and turns to 0 if pressed that direction
+        #up is the direction of the wires on the joystick
+        pressed = False
+        direction = 4
+        if ((GPIO.input(joystick[0]) == True) and (GPIO.input(joystick[1]) == True) \
+            and (GPIO.input(joystick[2]) == True) and (GPIO.input(joystick[3]) == True)):
+            sleep(0.05) #this section ensures it doesn't move 2 in one direction
+            
+            while (not pressed):      
+                for i in range(len(joystick)):
+                    while (GPIO.input(joystick[i]) == False):
+                        direction = i
+                        pressed = True
+
+                if (GPIO.input(buttons[0]) == True):
+                    sounds[1].stop()
+                    sleep(0.5)
+                    titleStatus = 1
+                    window.destroy()
+                    gameloop()
+
+            if (direction == 3): #UP
+                if (currenty != 1):
+                    b1.blue(currentx, currenty) #change the previous square to blue
+                    currentx += 0
+                    currenty += -1
+                    b1.green(currentx, currenty) #change the new square to green
+
+            if (direction == 2): #DOWN
+                if (currenty != 8):
+                    b1.blue(currentx, currenty)
+                    currentx += 0
+                    currenty += 1
+                    b1.green(currentx, currenty)
+
+            if (direction == 1): #LEFT
+                if (currentx != 1):
+                    b1.blue(currentx, currenty)
+                    currentx += -1
+                    currenty += 0
+                    b1.green(currentx, currenty)
+
+            if (direction == 0): #RIGHT
+                if (currentx != 8):
+                    b1.blue(currentx, currenty)
+                    currentx += 1
+                    currenty += 0
+                    b1.green(currentx, currenty)
+
+            window.update_idletasks()
+            window.update()
+
+
         
 ###MAIN CODE###
-#default screen size
-WIDTH = 765
-HEIGHT = 450
 
 #initialize pygame
 pygame.init()
 
 #setup switches and sounds
 joystick = [18, 19, 20, 21]
+buttons = [25]
 sounds = [
         pygame.mixer.Sound("gamefiles/battleshiptitle.wav"),
         pygame.mixer.Sound("gamefiles/suge.wav")
@@ -175,83 +297,15 @@ sounds[0].set_volume(0.5)
 #setup the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(joystick, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-#initial dictionary storing status (hit, miss, or regular) of every square "xy" in grid
-squareStatus = {}
-
-#initialize the window        
-window = Tk()
-window.geometry("{}x{}".format(WIDTH, HEIGHT))
-window.title("Battleship")
-window.configure(background="light gray")
-
-#create an instance of the game
-b1 = Game(window)
-b1.setupGUI()
-
-#make an initial square to make green
-currentx = 4
-currenty = 4
-b1.green(currentx, currenty)
-
-#start the title music
-sounds[0].play(loops=-1)
-
-#render the GUI in the main loop
-#window.mainloop()
-window.update_idletasks() #same as mainloop but allows frame to update
-window.update()
+GPIO.setup(buttons, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 
+#makes title only appear once
+titleStatus = 0
+
+#initialize window
+title = Tk()
 
 ###LOOP OF GAME###
-while True:
-
-    #test if the joystick is being moved any direction and go that direction
-    #joystick is normally 1 and turns to 0 if pressed that direction
-    #up is the direction of the wires on the joystick
-    pressed = False
-    if ((GPIO.input(joystick[0]) == True) and (GPIO.input(joystick[1]) == True) \
-        and (GPIO.input(joystick[2]) == True) and (GPIO.input(joystick[3]) == True)):
-        sleep(0.05) #this section ensures it doesn't move 2 in one direction
-        
-        while (not pressed):      
-            for i in range(len(joystick)):
-                while (GPIO.input(joystick[i]) == False):
-                    direction = i
-                    pressed = True
-
-        if (direction == 3): #UP
-            if (currenty != 1):
-                b1.blue(currentx, currenty) #change the previous square to blue
-                currentx += 0
-                currenty += -1
-                b1.green(currentx, currenty) #change the new square to green
-
-        if (direction == 2): #DOWN
-            if (currenty != 8):
-                b1.blue(currentx, currenty)
-                currentx += 0
-                currenty += 1
-                b1.green(currentx, currenty)
-
-        if (direction == 1): #LEFT
-            if (currentx != 1):
-                b1.blue(currentx, currenty)
-                currentx += -1
-                currenty += 0
-                b1.green(currentx, currenty)
-
-        if (direction == 0): #RIGHT
-            if (currentx != 8):
-                b1.blue(currentx, currenty)
-                currentx += 1
-                currenty += 0
-                b1.green(currentx, currenty)
-
-        window.update_idletasks()
-        window.update()
-
-
-    
+gameloop()
 
