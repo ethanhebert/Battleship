8faa3565530447218c3092d0f1ec2fc187ab7f88
ethanhebert <80844614+ethanhebert@users.noreply.github.com>
2021-05-09 19:25:28 -0500

    Add files via upload

--- Battleship Code P2.py
+++ Battleship Code P2.py
@@ -1,3 +1,11 @@
+##############################################################
+# Names: Ethan Hebert & Jace Peloquin
+# Date: 5-11-21
+# Description: This code is the Player 2 component of a
+# video game recreation of the famous Battleship boardgame.
+# Louisiana Tech CSC-132 Final project.
+##############################################################
+
 #here we go
 #PLAYER 2
 
